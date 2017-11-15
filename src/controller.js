8d12fc65b0dd3453a2c8d4337fae61efd70f75dd
Leon Brett <leon_brett@live.co.uk>
2017-11-15 22:59:01 +0000

    Adding display and touch events

--- src/controller.js
+++ src/controller.js
@@ -7,13 +7,20 @@ export default class VirtualJoyStick {
     constructor(options) {
         let defaults = {
             timeout: 250,
-            'button-color': '#FFFFFF',
-            'track-color': '#AAAAAA',
-            'button-size': 30,
-            'track-size': 50,
+            'button-color': '#FF0000',
+            'track-color': '#00FF0099',
+            'button-size': 100,
+            'track-size': 150,
+            'tracking-element': document.body,
+            'factor': 0.01,
         }
-        this.options = Object.assign({}, options, defaults);
+        this.options = Object.assign({}, defaults, options);
+        this._isAttached = false;
+        this.x = 0;
+        this.y = 0;
         this.createCanvas();
+        this.bindEvents();
+        this.show();
     }
     /**
      * Creates the canvas
@@ -21,8 +28,89 @@ export default class VirtualJoyStick {
      * @memberof VirtualJoyStick
      */
     createCanvas() {
-        this.canvas = new Canvas();
-        this.context = this.canvas.getContext('2D');
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.options['track-size'];
+        this.canvas.height = this.options['track-size'];
+        this.context = this.canvas.getContext('2d');
+    }
+
+    bindEvents() {
+        let el = this.options['tracking-element'];
+        el.addEventListener('touchstart', (ev) => this.start(ev), false);
+        el.addEventListener('touchmove', (ev) => this.move(ev), false);
+        el.addEventListener('touchend', (ev) => this.end(ev), false);
+    }
+
+    start(event) {
+        if(!this._isAttached) return;
+
+        if(event.targetTouches.length == 1) {
+            var touch = event.targetTouches[0];
+            this.currentTouch = {
+                identifier: touch.identifier,
+                x:touch.pageX,
+                y:touch.pageY
+            };
+
+            this.x = 0;
+            this.y = 0;
+        }
+        event.preventDefault();
+        return false;
+    }
+
+    getAngle(x, y) {
+        if(x == 0) {
+            return 0;
+        }
+        return Math.atan(y/x);
+    }
+    findTouch(touches) {
+        for(var index in touches) {
+            if(touches[index].identifier == this.currentTouch.identifier) {
+                return touches[index];
+            }
+        }
+        return false;
+    }
+    move(event) {
+        if(!this._isAttached) return;
+        if(!this.currentTouch) return;
+        let changedTouch = this.findTouch(event.changedTouches);
+        
+        if(changedTouch) {
+            let changeX = changedTouch.pageX - this.currentTouch.x;
+            let changeY = changedTouch.pageY - this.currentTouch.y;
+            
+            let fx = changeX * this.options['factor'];
+            let fy = changeY * this.options['factor'];
+
+
+            let px =  this.x + fx;
+            let py = this.y + fy
+
+            let rads = this.getAngle(px, py);
+            let maxX = Math.abs(Math.cos(rads));
+            let maxY = Math.abs(Math.sin(rads));
+
+            this.x = Math.min(maxX, px);
+            this.y = Math.min(maxY, py);
+            this.x = Math.max(-maxX,this.x);
+            this.y = Math.max(-maxY,this.y);
+
+            this.currentTouch.x = changedTouch.pageX;
+            this.currentTouch.y = changedTouch.pageY;
+        }
+
+        event.preventDefault();
+        return false;
+    }
+
+    end(event) {
+        this.currentTouch = null;
+        this.x = 0;
+        this.y = 0;
+        return false; 
     }
 
     /**
@@ -42,7 +130,7 @@ export default class VirtualJoyStick {
      */
     show() {
         this._isAttached = true;
-        document.body.appendChild(this.canvas);
+        this.options['tracking-element'].appendChild(this.canvas);
     }
     /**
      * Renders a frame
@@ -56,14 +144,16 @@ export default class VirtualJoyStick {
         this.drawCircle(
             this.options['track-size'] / 2,
             this.options['track-size'] / 2,
-            this.options['track-size'],
+            this.options['track-size'] / 2,
             this.options['track-color']
         );
 
+        let trackRange = (this.options['track-size'] - this.options['button-size']) / 2;
+
         this.drawCircle(
-            this.options['track-size'] / 2,
-            this.options['track-size'] / 2,
-            this.options['button-size'],
+            (this.options['track-size'] / 2) + (this.x * trackRange),
+            (this.options['track-size'] / 2) + (this.y * trackRange),
+            this.options['button-size'] / 2,
             this.options['button-color']
         );
     }
@@ -77,8 +167,10 @@ export default class VirtualJoyStick {
      * @memberof VirtualJoyStick
      */
     drawCircle(x, y, radius, fillColor) {
+        this.context.beginPath();
         this.context.arc(x,y,radius, 0, 2 * Math.PI, false);
         this.context.fillStyle = fillColor;
+        this.context.lineWidth = 0;
         this.context.fill();
     }
 }
