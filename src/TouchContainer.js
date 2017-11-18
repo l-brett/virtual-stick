3d21e60867df55f4a3db0f39b8da153eca02d7e1
Leon Brett <leon_brett@live.co.uk>
2017-11-18 12:30:32 +0000

    Don't render touchContainer

--- src/TouchContainer.js
+++ src/TouchContainer.js
@@ -2,10 +2,10 @@ export default class TouchContainer {
     constructor(options) {
         let defaults = {
             'container': document.body,
-            'width': 100,
-            'height': 100,
+            'width': 50,
+            'height': 50,
             'left': 0,
-            'top':0
+            'top':50
         };
         this.options = Object.assign({}, defaults, options);
         this.createElement();
@@ -19,6 +19,13 @@ export default class TouchContainer {
         this.el.style.left = this.options.left + 'vw';
         this.el.style.top = this.options.top + 'vh';
         this.el.style.background= "#999999AA";
+        
+    }
+
+    show() {
         this.options.container.appendChild(this.el);
+    }
+    hide() {
+        this.options.container.removeChild(this.el);
     }
 }
